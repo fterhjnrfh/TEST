c6038c2d0b30c6e938e9948d20e2279db15d8b28
fterhjnrfh <2645376726@qq.com>
2026-03-04 18:00:28 +0800

    second

--- SignalSystem/src/SignalSystem.Contracts/obj/Debug/net8.0/SignalSystem.Contracts.AssemblyInfo.cs
+++ SignalSystem/src/SignalSystem.Contracts/obj/Debug/net8.0/SignalSystem.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalSystem.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bd2ab73e0f6979c730b56ee72d9af92883ddb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalSystem.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalSystem.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
